ca9a42172012639d436cf45a664213057727f03b
Skarli Pena <skarlicodemusic@gmail.com>
2018-07-12 11:59:46 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var todo = new Vue({
+  el: '#todo',
+  data: {
+    newTask:"",
+    todoList: []
+  
+  },
+  methods: {
+      addTask:function() {
+        var task = this.newTask;
+        this.todoList.push(task)
+        this.newTask ="";
+      },
+      removeTask: function(task) {
+        var index = this.todoList.indexOf(task);
+        this.todoList.splice(index, 1)
+      }
+    // btnClick: function() {
+
+      // console.log(this.numer * 2);
+      // this.isUser = !this.isUser;
+    }
+  });
